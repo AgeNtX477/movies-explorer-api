46acce37bf802c1b82a7f920ce42af1812e9b5a1
Jury Obuhov <jury2500@gmail.com>
2022-12-23 17:41:54 +0300

    level-1 total done

---
+++ controllers/movies.js
@@ -0,0 +1,63 @@
+const Movie = require('../models/movie');
+const BadRequestErr = require('../errors/BadRequestErr');
+const NotFoundErr = require('../errors/NotFoundErr');
+const ForbiddenErr = require('../errors/ForbiddenErr');
+const ConflictErr = require('../errors/ConflictErr');
+
+module.exports.getMovies = (req, res, next) => {
+  const owner = req.user._id;
+  Movie.find({ owner })
+    .then((movie) => {
+      if (!movie) {
+        throw new NotFoundErr('Фильмы не найдены');
+      }
+      return res.status(200).send({ data: movie });
+    })
+    .catch(next);
+};
+
+module.exports.postMovie = (req, res, next) => {
+  const {
+    country, director, duration, year, description, image,
+    trailerLink, nameRU, nameEN, thumbnail, movieId,
+  } = req.body;
+  Movie.create({
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    nameRU,
+    nameEN,
+    thumbnail,
+    movieId,
+    owner: req.user._id,
+  })
+    .then((newMovie) => res.send({ data: newMovie }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        throw new BadRequestErr('Переданы некорректные данные при создании фильма');
+      } else if (err.code === 11000) {
+        throw new ConflictErr('Запрос не может быть выполнен из-за конфликтного обращения к ресурсу');
+      }
+    })
+    .catch(next);
+};
+
+module.exports.deleteMovie = (req, res, next) => {
+  const owner = req.user._id;
+  Movie.findById(req.params.movieId)
+    .then((movie) => {
+      if (!movie) {
+        throw new NotFoundErr('Фильм не найден');
+      }
+      if (movie.owner.toString() !== owner) {
+        throw new ForbiddenErr('Вы можете удалять только свои фильмы');
+      } else {
+        return movie.remove().then(() => res.status(200).send({ data: movie }));
+      }
+    })
+    .catch(next);
+};
---
+++ errors/UnAuthErr.js
@@ -0,0 +1,7 @@
+class UnAuthErr extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 401;
+  }
+}
+module.exports = UnAuthErr;
